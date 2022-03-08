8baaec9ebd91d8be31487c32d4ebedf51d3e094c
kavin t <ksbkthirukonda@gmail.com>
2022-03-08 01:07:29 -0500

    dunst, dwm alpha

--- install-alt-path.sh
+++ install-alt-path.sh
@@ -5,6 +5,9 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 mkdir -p ~/.config/nvim/
 ln -f -s $SCRIPT_PATH/.nvimrc ~/.config/nvim/init.vim
 
+mkdir -p ~/.config/dunst/
+ln -f -s $SCRIPT_PATH/.dunstrc ~/.config/dunst/dunstrc
+
 # install plugins
 export PATH=$PATH:/snap/bin
 nvim -c "PlugInstall --sync" -c "PlugUpdate --sync" -c "qall"
